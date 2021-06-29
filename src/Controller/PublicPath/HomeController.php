35d02f74e357e72991a1049dfb4a6e0d8b88c353
Allin RAMANAMPISOA <Aallin13ramanampisoa@outlook.fr>
2021-06-29 12:05:00 +0300

    Temp

--- src/Controller/PublicPath/HomeController.php
+++ src/Controller/PublicPath/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\PublicPath;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +17,19 @@ class HomeController extends AbstractController
     {
         return $this->render('public_path/home/homePage.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/test-article", name="public_path_testArticle")
+     */
+    public function testArticle(): Response
+    {
+        $article = new Article();
+        $articleForm = $this->createForm(ArticleType::class, $article);
+
+        return $this->render('public_path/my_test/index.html.twig', [
+            'articleForm' => $articleForm->createView(),
         ]);
     }
 }
